94519f4d4191055ebd186984334385cad4d9510e
Mikhail_Zakharov <corsar6599442@gmail.com>
2020-06-07 20:33:53 +0300

    Добавил кнопку

--- javascript/arrays/chick.js
+++ javascript/arrays/chick.js
@@ -1,8 +1,8 @@
 var cickIncSched = {
     date: '',
-    day: '',
-    temperature: '',
-    humidity: '',
-    turn: '',
-    cooling: ''
+    day: '0',
+    temperature: '37.8',
+    humidity: '55',
+    turn: 'Да',
+    cooling: 'Нет'
 }
